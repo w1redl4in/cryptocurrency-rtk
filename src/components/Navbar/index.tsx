0667173e371908df459310fb3ef54e292f5eb283
Felipe <felipe1180@hotmail.com>
2022-01-09 15:11:14 -0300

    feat: :sparkles: navbar component

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,37 +1,50 @@
-import { Text, Tabs, Display, Image } from '@geist-ui/react';
+import { Text, Tabs, Display, Image, Page } from '@geist-ui/react';
+import { Dashboard } from '@components/Dashboard';
+import { ToggleTheme } from './ToggleTheme';
 
 export const Navbar = () => {
   const tabs = [
     {
       label: 'Dashboard',
       value: 'Dashboard',
+      Component: Dashboard,
+      disabled: false,
     },
     {
       label: 'Cryptocurrencies',
       value: 'Cryptocurrencies',
+      disabled: true,
     },
     {
       label: 'Exchanges',
       value: 'Exchanges',
+      disabled: true,
     },
     {
       label: 'News',
       value: 'News',
+      disabled: true,
     },
   ];
 
   return (
-    <>
+    <Page.Header>
+      <ToggleTheme />
       <Display caption={<Text h2>Cryptocurrency</Text>}>
         <Image width="100px" src="/nft.svg" />
       </Display>
-      <Tabs initialValue="Dashboard">
+      <Tabs hideDivider initialValue="Dashboard">
         {tabs.map((tab) => (
-          <Tabs.Item label={tab.label} value={tab.value}>
-            <Text>{tab.value}</Text>
+          <Tabs.Item
+            disabled={tab.disabled}
+            label={<Text b>{tab.label}</Text>}
+            value={tab.value}
+            key={tab.label}
+          >
+            {tab.Component && <tab.Component />}
           </Tabs.Item>
         ))}
       </Tabs>
-    </>
+    </Page.Header>
   );
 };
