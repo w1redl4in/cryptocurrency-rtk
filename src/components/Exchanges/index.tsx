ac39951081b55297638b6723f211dbfab50a3088
Felipe <felipe1180@hotmail.com>
2022-01-10 19:55:04 -0300

    feat: :sparkles: exchange tab

--- src/components/Exchanges/index.tsx
+++ src/components/Exchanges/index.tsx
@@ -1,17 +1,14 @@
-import { Card, Loading, Page } from '@geist-ui/react';
+import { Grid, Text } from '@geist-ui/react';
 import { useGetExchangesQuery } from '@services/apis/coinranking/endpoints/exchanges';
 
 export const Exchanges = () => {
-  const { isLoading, data } = useGetExchangesQuery();
-
-  const fetchedData = data?.data;
+  const {} = useGetExchangesQuery();
 
   return (
-    <Page.Content>
-      {isLoading && <Loading />}
-      {fetchedData?.exchanges.map((exchange) => (
-        <Card key={exchange.uuid}>{exchange.name}</Card>
-      ))}
-    </Page.Content>
+    <Grid.Container justify="center" alignItems="center" mt={5}>
+      <Text h3 b>
+        Coinranking API does not allow this endpoint{' '}
+      </Text>
+    </Grid.Container>
   );
 };
