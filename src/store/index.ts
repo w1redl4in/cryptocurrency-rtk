5f9c82fbf8415d10822bbb637fbc849f046756d0
Felipe <felipe1180@hotmail.com>
2022-01-10 14:53:56 -0300

    feat: :sparkles: useAppDispatch type

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import { fetchCoinByUuid, getCoins } from '@services/endpoints';
 import theme from '@store/theme/slices';
 import coin from '@store/coins/slice';
+import { useDispatch } from 'react-redux';
 
 const configStore = configureStore({
   reducer: {
@@ -13,4 +14,7 @@ const configStore = configureStore({
 });
 
 export type AppState = ReturnType<typeof configStore.getState>;
+
+export type AppDispatch = typeof configStore.dispatch;
+export const useAppDispatch = () => useDispatch<AppDispatch>();
 export default configStore;
